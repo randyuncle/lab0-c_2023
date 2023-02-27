349b4e9cc982065fb2a8b9ad46d5e2496bcc9bdc
randyuncle <randylee609@gmail.com>
2023-02-27 18:42:52 +0800

    Implement function q_sort (and function divide, merge_two_list for it), q_descend, q_merge. Fix memory leak in function q_reverseK

--- queue.c
+++ queue.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -183,41 +184,129 @@ void q_reverseK(struct list_head *head, int k)
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
     if (!head || list_empty(head))
         return;  // `head` is NULL, or there's no list in `head`
-    struct list_head *iterator, *next, *start = head;
-    struct list_head *dummy =
-        (struct list_head *) malloc(sizeof(struct list_head));
-    if (!dummy)
-        return;             // no space for dummy
-    INIT_LIST_HEAD(dummy);  // pointer dummy serve as same as pointer head does
+    struct list_head *iterator, *next, *start = head, dummy;
+    INIT_LIST_HEAD(
+        &dummy);  // pointer dummy serves as same as pointer head does
     int i = 0;
     list_for_each_safe (iterator, next, head) {
         i++;
         if (i < k)
             continue;
-        list_cut_position(dummy, start,
+        list_cut_position(&dummy, start,
                           iterator);  // cut k node of the list out as an
                                       // independent list to be reverse
-        q_reverse(dummy);
-        list_splice_init(dummy, start);  // take dummy back to the original list
+        q_reverse(&dummy);
+        list_splice_init(&dummy,
+                         start);  // take dummy back to the original list
         start = next->prev;
+        i = 0;
     }
-    list_del(dummy);  // free the memory space
+}
+
+/* Merging two independent list */
+struct list_head *merge_two_list(struct list_head *left,
+                                 struct list_head *right)
+{
+    // reference: 你所不知道的 C 語言: linked list 和非連續記憶體
+    struct list_head *head = NULL;
+    struct list_head **p = &head;
+    for (; left && right; p = &((*p)->next)) {
+        element_t *l = list_entry(left, element_t, list);
+        element_t *r = list_entry(right, element_t, list);
+        if (strcmp(l->value, r->value) < 0) {
+            *p = left;
+            left = left->next;
+        } else {
+            *p = right;
+            right = right->next;
+        }
+    }
+    *p = (struct list_head *) ((uintptr_t) left |
+                               (uintptr_t) right);  // <stdint.h>
+    return head;
+}
+
+/* Doing the list seperation for merge sort (divide and conquer) */
+struct list_head *divide(struct list_head *head, struct list_head *end)
+{
+    if (head == end)
+        return head;
+    // find middle point
+    struct list_head *foreward = head, *backward = end;
+    for (; foreward != backward && foreward->next != backward;
+         foreward = foreward->next, backward = backward->prev)
+        ;
+    if (foreward == backward)
+        backward = backward->next;
+    // make the list no longer be circular
+    foreward->next = NULL;
+    backward->prev = NULL;
+    // keep divide until hit the break point
+    struct list_head *left = divide(head, foreward);
+    struct list_head *right = divide(backward, end);
+    // conquer the partitions
+    return merge_two_list(left, right);
 }
 
 /* Sort elements of queue in ascending order */
-void q_sort(struct list_head *head) {}
+void q_sort(struct list_head *head)
+{
+    // merge sort machemic
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;  // `head` is NULL, no list in `head`, or one element
+    struct list_head *end = head->prev;
+    // make the list no longer be circular
+    end->next = NULL;
+    head->next->prev = NULL;
+    head->next = divide(head->next, end);
+    // make the list to be circular again (move the head back)
+    struct list_head *curr;
+    for (curr = head; curr->next; curr = curr->next)
+        curr->next->prev = curr;
+    curr->next = head;
+    curr->next->prev = curr;
+}
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
 int q_descend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;  // `head` is NULL, or there's no list in `head`
+    // this section cares about value in 'element_t' structure
+    struct list_head *curr = head->prev;
+    element_t *p, *c_max = list_entry(curr, element_t, list);
+    for (; c_max->list.prev != head;) {
+        p = list_entry(c_max->list.prev, element_t, list);
+        if (strcmp(p->value, c_max->value) <= 0) {
+            list_del(&p->list);
+            q_release_element(p);
+        } else
+            c_max = p;
+    }
+    return q_size(head);
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending order */
 int q_merge(struct list_head *head)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;  // `head` is NULL, or there's no list in `head`
+    if (list_is_singular(head))
+        return list_entry(head->next, queue_contex_t, chain)->size;
+    // merging by 'q_context_t' structure
+    queue_contex_t *main_q = list_entry(head->next, queue_contex_t, chain);
+    // 分段合併
+    for (struct list_head *curr = head->next->next; curr != head;
+         curr = curr->next) {
+        queue_contex_t *c = list_entry(curr, queue_contex_t, chain);
+        list_splice_init(c->q, main_q->q);
+        main_q->size += c->size;
+        c->size = 0;
+    }
+    // sorting
+    q_sort(main_q->q);
+    return main_q->size;
 }
